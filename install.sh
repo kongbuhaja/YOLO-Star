69d4a6f94929423efa5a712ecfb09bd3afc291b3
kongbuhaja <pcar530@gmail.com>
2025-05-15 16:44:18 +0900

    update docker

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu122
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install ultralytics
