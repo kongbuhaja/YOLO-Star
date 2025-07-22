d97a3e007108a84ffe0f34f02a7342f88ab0aaac
kongbuhaja <pcar530@gmail.com>
2025-07-22 19:02:24 +0900

    update for test

--- train.py
+++ train.py
@@ -4,10 +4,12 @@ import os, psutil
 parser = argparse.ArgumentParser(description='DeCo-YOLOs')
 parser.add_argument('--model', dest='model', type=str, default='yolov10n')
 parser.add_argument('--data', dest='data', type=str, default='coco')
-parser.add_argument('--batch_size', dest='batch_size', type=int, default=64, help='n=64, x=8')
+parser.add_argument('--batch_size', dest='batch_size', type=int, default=16, help='n=64, x=8')
+parser.add_argument('--workers', dest='workers', type=int, default=12, help='threads')
 parser.add_argument('--gpus', dest='gpus', type=str, default='0', help='which device do you want to use')
 parser.add_argument('--cpus', dest='cpus', type=str, default='0-23', help='how many cores do you want to use')
-parser.add_argument('--epochs', dest='epochs', type=int, default=1000)
+parser.add_argument('--epochs', dest='epochs', type=int, default=500)
+parser.add_argument('--lr', dest='lr', type=float, default='0.02', help='start learning rate')
 
 args = parser.parse_args()
 
@@ -33,6 +35,7 @@ model_file = args.model if '.pt' in args.model else f'{args.model}.yaml'
 model = YOLO(f"{model_file}").to(device)
 
 train_args = dict(
+    workers = args.workers,
     data = f'{args.data}.yaml',
     batch=args.batch_size,
     epochs=args.epochs,
@@ -40,6 +43,7 @@ train_args = dict(
     device=device,
     val=True,
     save_period=1,
-    name=args.model
+    name=args.model,
+    lr0=args.lr
 )
 train_results = model.train(**train_args)
